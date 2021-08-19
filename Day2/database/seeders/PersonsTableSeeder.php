1ca03247b74a1a3da257bd8fbad58bb9a766bc47
DebanganThakuria <debangan.thakuria@razorpay.com>
2021-08-20 00:22:44 +0530

    Boiler Plate code for Day_3 exercise

---
+++ Day2/database/seeders/PersonsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PersonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        echo "Seedling!";
+    }
+}
